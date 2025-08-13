6e749e081f0f8cd0c670e3571d6224b4c915158b
Jake Hathaway <jake.d.hathaway@gmail.com>
2025-08-13 14:00:09 -0700

    update headers

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
-// main.c
 #include "common.h"
+#include "chunk.h"
 #include <stdio.h>
 
-int main(void) {
-  Random thing = {.value = 1}; // designated initializer in C
+int main(void)
+{
+  Random thing = {.value = 1};
   printf("Hello, value = %zu!\n", thing.value);
   return 0;
 }
